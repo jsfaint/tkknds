d1994570c670f0813a1dce76decec25aa0e31836
jsfaint <jsfaint@ecffb882-c1c8-11dd-874b-8bacd04a0a74>
2008-12-04 12:07:38 +0000

    Remove these automatically c file.

--- trunk/source/gfx/plane.pal.c
+++
@@ -1,4 +0,0 @@
-//Palette created using Mollusk's PAGfxConverter
-
-const unsigned short plane_Pal[11] __attribute__ ((aligned (4))) = {
-64543, 32768, 65535, 58136, 65504, 64512, 64607, 49680, 32799, 33791, 32784};
