224d30cb5b986c12b43164899f81fd48ffefa047
jsfaint <jsfaint@ecffb882-c1c8-11dd-874b-8bacd04a0a74>
2008-12-05 13:46:37 +0000

    fix collision check issue.

--- trunk/source/game.c
+++ trunk/source/game.c
@@ -165,7 +165,7 @@ bool bCheckCollision()
     s16 ii;
     for (ii=0; ii<BULLET_MAX; ii++)
     {
-        if (PA_Distance(g_Plane.x+8, g_Plane.y+8, g_bullet[ii].x+2, g_bullet[ii].y+2) < 8*2)
+        if (PA_Distance(g_Plane.x+8, g_Plane.y+8, g_bullet[ii].x+4, g_bullet[ii].y+4) < 8*2)
             return TRUE;
     }
 
