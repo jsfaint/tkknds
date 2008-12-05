8cc2ab102f4cdaed4dea9b641ea546356529a29c
jsfaint <jsfaint@ecffb882-c1c8-11dd-874b-8bacd04a0a74>
2008-12-05 00:50:53 +0000

    modify environment variables, to solve the conflict on some computers.

--- trunk/clean.bat
+++ trunk/clean.bat
@@ -1,5 +1,5 @@
 @echo off
-set PATH=%PATH%;d:\devkitPro\msys\bin;d:\devkitPro\devkitARM\bin;
+set PATH=d:\devkitPro\msys\bin;d:\devkitPro\devkitARM\bin;
 set DEVKITPRO=/d/devkitPro
 set DEVKITARM=/d/devkitPro/devkitARM
 set PAPATH=/d/devkitPro/PALib/lib
