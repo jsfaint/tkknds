91139caceea384a40057b024b6b323800b71981c
jsfaint <jsfaint@ecffb882-c1c8-11dd-874b-8bacd04a0a74>
2008-12-06 03:11:00 +0000

    Modify restart key to 'Any key' or 'Stylus'.

--- trunk/source/main.c
+++ trunk/source/main.c
@@ -55,7 +55,7 @@ int main(int argc, char ** argv)
             break;
         
         case Game_Statis:
-            if(Pad.Newpress.L || Pad.Newpress.R || Stylus.Newpress)
+            if(Pad.Newpress.Anykey || Stylus.Newpress)
                 gameState = Menu_Init;
             break;
         default:
