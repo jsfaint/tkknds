9d2b0ca7736604b6335d1f6619c31d905aa37598
jsfaint <jsfaint@ecffb882-c1c8-11dd-874b-8bacd04a0a74>
2010-08-18 02:19:17 +0000

    Add temp file to ignore list...

--- clean.bat
+++ clean.bat
@@ -1,11 +1,10 @@
 @echo off
 rem I just configure the environment variables what I need.
-rem set PATH=%PATH%;d:\devkitPro\msys\bin;d:\devkitPro\devkitARM\bin;
-set PATH=d:\msys\bin;h:\nds\devkitPro\devkitARM\bin;
-set DEVKITPRO=/h/nds
-set DEVKITARM=/h/nds/devkitARM
+set PATH=e:\nds\msys\bin;e:\nds\devkitPro\devkitARM\bin;
+set DEVKITPRO=/e/nds
+set DEVKITARM=/e/nds/devkitARM
 rem using PALib
-set PAPATH=/h/nds/PALib/lib
+set PAPATH=/e/nds/PALib/lib
 @echo on
 
 make clean
